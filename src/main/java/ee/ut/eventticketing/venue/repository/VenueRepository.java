437b5edb261a0bc6f6c33b9273acf83978979b05
unknown <you@example.com>
2026-05-19 04:35:10 +0300

    Update venue service with JWT security

--- src/main/java/ee/ut/eventticketing/venue/repository/VenueRepository.java
+++
@@ -1,9 +0,0 @@
-package ee.ut.eventticketing.venue.repository;
-
-import ee.ut.eventticketing.venue.model.Venue;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VenueRepository extends JpaRepository<Venue, Long> {
-}
